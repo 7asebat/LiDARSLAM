c02110101b917f2336d34c40f3fc181bf85a14df
Youssef Walid <yousefwalid98@hotmail.com>
2022-05-28 12:16:40 +0200

    Get repo up to speed

---
+++ README.md
@@ -0,0 +1,95 @@
+# SLAMEnvironment
+
+Vehicle model with (3D lidar, IMU and Camera sensors) can be simulated in 2 different maps.
+![ Vehicle ](https://github.com/BelalElhossany/SLAMEnvironment/blob/main/images/vehicle.PNG)
+
+# How to run
+
+1- Install ROS
+  - melodic for UBUNTU 18
+  - noetic for UBUNTU 20
+ ```
+ http://wiki.ros.org/ROS/Installation
+ ```
+ NOTE: Choose Desktop-Full Install in section 1.4 to install gazebo with ROS
+ 
+ 2- Create catkin workspace
+ NOTE: don't follow install catkin link, you don't have to explicitly install it.
+ ```
+ http://wiki.ros.org/catkin/Tutorials/create_a_workspace
+ ```
+ 
+ 3- Clone the repo
+ ```
+ cd ~/catkin_ws/src
+ git clone https://github.com/BelalElhossany/SLAMEnvironment.git
+ ```
+ 
+ 4- Install dependencies
+ ```
+ cd ~/catkin_ws/src/SLAMEnvironment/vehicle_sim
+ rosdep install --from-paths . -y
+ ```
+ 
+ 5- Build
+ ```
+ catkin_make
+ source ~/catkin_ws/devel/setup.bash
+ ```
+ 6- upgrade gazebo version and download gazebo models
+ ```
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/gazebo-stable.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2486D2DD83DB69272AFE98867170598AF249743
+sudo apt update
+sudo apt upgrade
+ ```
+ ```
+ rosrun vehicle_sim_launcher setup.sh
+ ```
+ 
+ 7- Make python code executable
+ ```
+ cd ~/catkin_ws/src/SLAMEnvironment/slam_code/scripts
+ chmod 777 slamcode.py
+ ```
+ 8- Run Simple walls environment
+ ```
+ roslaunch vehicle_sim_launcher walls.launch
+ ```
+ ![ Walls ](https://github.com/BelalElhossany/SLAMEnvironment/blob/main/images/walls.PNG)
+ 
+ 9- Run complex city environment
+ ```
+ roslaunch vehicle_sim_launcher city.launch
+ ```
+ ![ City ](https://github.com/BelalElhossany/SLAMEnvironment/blob/main/images/city.PNG)
+ 
+ NOTE: add ```gpu:=true ``` to roslaunch command to use gpu.
+
+-------------------------------------------------------------------------------------------
+Once you launch it, 3 windows will pop up:
+  - Gazebo Simulation.
+  - RVIZ : For visualization, you will find:
+    1- Car model.
+    2- Colored points representing lidar readings.
+    3- Funny arrow representing IMU readings.
+    4- Image to show camera output.
+    Your map needs to be added to the list :)
+    
+    ![ Rviz ](https://github.com/BelalElhossany/SLAMEnvironment/blob/main/images/rviz.PNG)
+  - Window to control vehicle velocity and steering.
+  - 
+    ![ Vehicle ](https://github.com/BelalElhossany/SLAMEnvironment/blob/main/images/control.PNG)
+
+-------------------------------------------------------------------------------------------
+# Sample code
+sample code exists in: ```~/catkin_ws/src/SLAMEnvironment/slam_code/scripts```
+  - It subscribes to ```/points_raw``` topic which lidar publishes on.
+  - Creates dumy PointCloud2 message to simulate the map which will be the output of the SLAM algorithm, then publishes it on ```/map``` topic.
+  - Your SLAM code needs to be added to a script like this.
+
+NOTE: This code is included in launch files, so runs once you do step 7 or 8.
+-------------------------------------------------------------------------------------------
+# Citation
+
+this is a modified version of this repo ```https://github.com/yukkysaito/vehicle_sim```.
--- slam_code/scripts/slamcode.py
+++ slam_code/scripts/slamcode.py
@@ -1,28 +1,123 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
-from sensor_msgs.msg import PointCloud2
-from fast
+import numpy as np
+from time import time
 
+# ROS dependencies
+from std_msgs.msg       import String
+from sensor_msgs.msg    import PointCloud2
+from geometry_msgs.msg  import Twist
+from geometry_msgs.msg  import Pose
+from geometry_msgs.msg  import PoseStamped
+from geometry_msgs.msg  import PoseArray
+from geometry_msgs.msg  import TransformStamped
+
+from tf.transformations import quaternion_from_euler
+from tf2_ros            import TransformBroadcaster
+
+import sensor_msgs.pointcloud2 as pc2
 
 def lidar_callback(data):
-    rospy.loginfo("I've received lidar data")
+    #points = np.array(pc2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True))
+    pass
 
+def normalize_angle(angle):
+    pass
+
+def pose_update():
+    global particles
+
+    pose_stamped = PoseStamped()
+    br = TransformBroadcaster()
+    t = TransformStamped()
+
+    # header   
+    pose_stamped.header.stamp    = t.header.stamp    = rospy.Time.now()
+    pose_stamped.header.frame_id = t.header.frame_id = "world"
+    t.child_frame_id = "base_footprint"
+
+    # position
+    particle = particles[0]
+    pose_stamped.pose.position.x = t.transform.translation.x = particle.pose['x']
+    pose_stamped.pose.position.y = t.transform.translation.y = particle.pose['y']
+    pose_stamped.pose.position.z = t.transform.translation.z = 0
+
+    # orientation
+    yaw = particle.pose['theta']
+    q = quaternion_from_euler(0, 0, yaw)
+    pose_stamped.pose.orientation.x = t.transform.rotation.x = q[0] # 0
+    pose_stamped.pose.orientation.y = t.transform.rotation.y = q[1] # 0
+    pose_stamped.pose.orientation.z = t.transform.rotation.z = q[2] # sin(yaw/2)
+    pose_stamped.pose.orientation.w = t.transform.rotation.w = q[3] # cos(yaw/2)
+
+    br.sendTransform(t)
+    return pose_stamped
+
+class Map:
+    def __init():
+        pass
+
+class Particle:
+    def __init__(self, num_particles, motion_noise, pose):
+        self.num_particles = num_particles
+        self.motion_noise = motion_noise
+        self.pose = pose
+        # self.map = Map(100, 100)
+
+    def predict(self, v, w, dt):
+        # sample control
+        v = np.random.normal(v, self.motion_noise['v'])
+        w = np.random.normal(w, self.motion_noise['w'])
+
+        # Apply motion model
+        self.pose['x'] += v * dt * np.cos(self.pose['theta'])
+        self.pose['y'] += v * dt * np.sin(self.pose['theta'])
+        self.pose['theta'] = normalize_angle(self.pose['theta'] + w * dt)
+
+    def correct(self):
+        pass
+
+def control_callback(data):
+    global particles, prev_time
+    current_time = time()
+    dt = current_time - prev_time # in sec
+    prev_time = current_time
+
+    if (dt > 1): # more than 1 sec
+        return
+
+    v = data.linear.x # in m/sec
+    w = data.angular.z # in rad/s
+
+    for particle in particles:
+        particle.predict(v, w, dt)    
+
+
+prev_time = time() # in sec
+motion_noise = { 'v': 0, 'w': 0 }
+initial_pose = { 'x': 0, 'y': 0, 'z': 0}
+num_particles = 1
+map_size = { 'x': 200, 'y': 200, 'RES': 1/4 } # send to the particle to create map
+particles = [Particle(num_particles, motion_noise, initial_pose) for _ in range(num_particles)]
 
 if __name__ == '__main__':
     try:
+        # Initialize SLAM code
         rospy.init_node('slam', anonymous=True)
-        rospy.Subscriber("/points_raw", PointCloud2, lidar_callback)
 
-        map_pub = rospy.Publisher('/map', PointCloud2, queue_size=10)
+        # Subscribers
+        rospy.Subscriber("/points_raw", PointCloud2, lidar_callback)
+        rospy.Subscriber("/cmd_vel",    Twist,       control_callback)
+ 
+        # Publishers
+        pose_pub = rospy.Publisher("/pose", PoseStamped, queue_size=10)
+        map_pub  = rospy.Publisher("/map",  PointCloud2, queue_size=10)
+
+        # Execute SLAM code
         rate = rospy.Rate(10)  # 10hz
         while not rospy.is_shutdown():
-            ############# SLAM CODE :) #####################
-            FastSLAM()
+            pose_pub.push(pose_update())
 
-            _map = PointCloud2()
-            _map.data = [1, 1, 1, 1, 1]
-            map_pub.publish(_map)
             rate.sleep()
 
     except rospy.ROSInterruptException:
