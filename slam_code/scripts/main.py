180f8a60a0f3872937a91f8d25c6709a6927a556
Youssef Walid <yousefwalid98@hotmail.com>
2022-05-31 18:45:22 +0200

    DEV: Finalize 2d code

--- slam_code/scripts/main.py
+++ slam_code/scripts/main.py
@@ -9,10 +9,9 @@ from time import time
 # ROS dependencies
 import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg    import PointCloud2, PointField
-from geometry_msgs.msg  import PointStamped, PoseStamped, Pose2D
+from geometry_msgs.msg  import PoseStamped, Pose2D
 from nav_msgs.msg import OccupancyGrid
 from skimage.draw import line
-from bresenham3d import Bresenham3D
 
 import tf2_ros
 import tf2_geometry_msgs
@@ -32,21 +31,20 @@ class Map:
         self.w = w # width
         self.h = h # height
 
-        sensor_model_p_occ = 0.75 # probability of occupancy in the sensor model
-        sensor_model_p_free = 0.45 # probability of free in the sensor model
-        sensor_model_p_prior = 0.5 # prior probability of occupancy in the sensor model
-        self.sensor_model_l_occ = p2l(sensor_model_p_occ) # log odds of occupancy in the sensor model
-        self.sensor_model_l_free = p2l(sensor_model_p_free) # log odds of free in the sensor model
-        self.sensor_model_l_prior = p2l(sensor_model_p_prior) # log odds of prior occupancy in the sensor model
+        p_occ = 0.75 # probability of occupancy in the sensor model
+        p_free = 0.45 # probability of free in the sensor model
+        p_prior = 0.5 # prior probability of occupancy in the sensor model
+        self.l_occ = prob_to_log(p_occ) # log odds of occupancy in the sensor model
+        self.l_free = prob_to_log(p_free) # log odds of free in the sensor model
+        self.l_prioir = prob_to_log(p_prior) # log odds of prior occupancy in the sensor model
 
-        self.map =  np.ones((w, h)) * self.sensor_model_l_prior
+        self.map =  np.ones((w, h)) * self.l_prioir
 
 class Particle:
-    def __init__(self, motion_noise, pose):
-        self.motion_noise = motion_noise
+    def __init__(self, pose):
         self.pose = pose
 
-def pose_2d_callback(data):
+def scan_matching_callback(data):
     global particle
     particle.pose['x'] = data.x
     particle.pose['y'] = data.y
@@ -72,36 +70,9 @@ def transform_point_to_basis(p):
 
     return np.array([pt.point.x, pt.point.y, pt.point.z])
 
-def get_points_between(p1, p2):
-    return Bresenham3D(p1, p2)
-    # diff = p2 - p1
-    # num_points = np.linalg.norm(diff)
-    # unit_diff = diff / num_points
-    # points = np.array([p1 + i * unit_diff for i in range(int(num_points + 1))]).astype(int)
-    # unique_points = np.unique(points, axis=0)
-    # return unique_points
-
 
 def normalize_and_publish_map():
-    grid_map_p = l2p(map.map)
-    grid_map_occupied = (grid_map_p * 127).astype(np.uint8)
-
-    occup_grid = OccupancyGrid()
-    occup_grid.info.width = map.w
-    occup_grid.info.height = map.h
-    occup_grid.data = grid_map_occupied.ravel()
-    occup_grid.header.stamp = rospy.Time.now()
-    occup_grid.header.frame_id = "world"
-    occup_grid.info.resolution = 1.0
-    occup_grid.info.origin.position.x = -map.w/2
-    occup_grid.info.origin.position.y = -map.h/2
-    occup_grid.info.origin.position.z = 0.0
-    occup_grid.info.origin.orientation.x = 0.0
-    occup_grid.info.origin.orientation.y = 0.0
-    occup_grid.info.origin.orientation.z = 0.0
-    occup_grid.info.origin.orientation.w = 1.0
-
-    map_pub.publish(occup_grid)
+    grid_map_p = log_to_prob(map.map)
     
     points = []
 
@@ -124,16 +95,13 @@ def normalize_and_publish_map():
     generated_pc2 = pc2.create_cloud(header, fields, points)
     map_pc2_pub.publish(generated_pc2)
 
-# p(x) = 1 - \frac{1}{1 + e^l(x)}
-def l2p(l):
-    return 1 - (1/(1+np.exp(l)))
+def log_to_prob(l):
+    return 1 - (1 / (1 + np.exp(l)))
 
-# l(x) = log(\frac{p(x)}{1 - p(x)})
-def p2l(p):
-    return np.log(p/(1-p))
+def prob_to_log(p):
+    return np.log(p / (1 - p))
 
-
-def lidar_callback(data):
+def lidar_reading_callback(data):
     global map
 
     data_gen = pc2.read_points(data, skip_nans=True, field_names=("x", "y", "z"))
@@ -152,33 +120,24 @@ def lidar_callback(data):
         if q[0] < 0 or q[0] > map.w or q[1] < 0 or q[1] > map.h:
             continue
 
+        # cast ray from robot to lidar point
         rr, cc = line(int(robot_pose[0]), int(robot_pose[1]), int(q[0]), int(q[1]))
 
         # miss
-        map.map[cc, rr] += map.sensor_model_l_free - map.sensor_model_l_prior
+        map.map[cc, rr] += map.l_free - map.l_prioir
 
         # hit, but need to subtract the added value above
-        map.map[cc[-1], rr[-1]] -= map.sensor_model_l_free - map.sensor_model_l_prior
-        map.map[cc[-1], rr[-1]] += map.sensor_model_l_occ - map.sensor_model_l_prior
+        map.map[cc[-1], rr[-1]] -= map.l_free - map.l_prioir
+        map.map[cc[-1], rr[-1]] += map.l_occ - map.l_prioir
 
         # constraint ranges ranges
         map.map[map.map > 100] = 100
         map.map[map.map < 0] = 0
 
-        # map.map[cc, rr] -= 1
-        # map.map[cc[-1], rr[-1]] += 2
-
-
-        # matched_points = get_points_between(np.array([robot_pose[0], robot_pose[1]]).astype(int), np.array([q[0], q[1]]).astype(int))
-        # map.map[matched_points[:, 1], matched_points[:, 0]] -= 1
-        # map.map[matched_points[-1, 1], matched_points[-1, 0]] += 2
-        
-
-
 prev_time = time() # in sec
-motion_noise = { 'v': 0, 'w': 0 }
 initial_pose = { 'x': 0, 'y': 0, 'theta': 0}
-particle = Particle(motion_noise, initial_pose)
+particle = Particle (initial_pose)
+
 map = Map()
 world_base_footprint_tf_buffer = None
 world_base_footprint_tf_listener = None
@@ -190,13 +149,12 @@ if __name__ == '__main__':
         rospy.init_node('slam', anonymous=True)
 
         # Subscribers
-        rospy.Subscriber("/pose2D", Pose2D,      pose_2d_callback)
-        rospy.Subscriber("/cloud",  PointCloud2, lidar_callback)
+        rospy.Subscriber("/pose2D", Pose2D,      scan_matching_callback)
+        rospy.Subscriber("/cloud",  PointCloud2, lidar_reading_callback)
  
         # Publishers
         cloud_pub = rospy.Publisher('/cloud', PointCloud2,   queue_size=10)
         pose_pub  = rospy.Publisher("/pose",  PoseStamped,   queue_size=10)
-        map_pub   = rospy.Publisher("/map",   OccupancyGrid, queue_size=10)
         map_pc2_pub = rospy.Publisher("/map_pc2", PointCloud2, queue_size=10)
 
         world_base_footprint_tf_buffer = tf2_ros.Buffer()
